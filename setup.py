a4f88b7bbfc4c479228f0464631f55212adf213e
Enrico Bagli <enrico.bagli@gmail.com>
2018-10-19 19:56:08 +0200

    minor

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(name='pychanneling',
-      version='0.1',
-      description='Analysis package for channeling experiments',
-      url='http://github.com/ebagli/pychanneling',
-      author='Enrico Bagli',
-      author_email='enrico.bagli@gmail.com',
-      license='MIT',
-      packages=['pychanneling'],
-      zip_safe=False)
+setuptools.setup(
+        name='pychanneling',
+        version='v0.1-alpha',
+        description='Analysis package for channeling experiments',
+        url='http://github.com/ebagli/pychanneling',
+        author='Enrico Bagli',
+        author_email='enrico.bagli@gmail.com',
+        license='MIT',
+        packages=setuptools.find_packages('pychanneling'),
+        classifiers=[
+                "Development Status :: 3 - Alpha",
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent"
+                ],
+        install_requires=[
+        'pandas',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'uproot'
+        ],
+        include_package_data=True,
+        zip_safe=False)
